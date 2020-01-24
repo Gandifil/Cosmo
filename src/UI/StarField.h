3f2708e0fda19eca3421e87b1bd58d8f45eea95f
Gandifil <Gandifil@DESKTOP-8DCQQ76>
2020-01-24 19:27:44 +0300

    Refactor StarField class. Delete .cpp file.

--- src/UI/StarField.h
+++ src/UI/StarField.h
@@ -1,23 +1,50 @@
-#pragma once
+//
+// Created by Gandifil on 24.01.2020.
+//
+
+#ifndef COSMO_UI_STARFIELD
+#define COSMO_UI_STARFIELD
+
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <random>
 
-namespace Cosmo
-{
-	namespace UI
+namespace Cosmo::UI {
+	class StarField: public sf::Drawable {
+	public:
+		StarField(sf::RenderWindow const &wnd);
+		inline virtual void draw(sf::RenderTarget &wnd, sf::RenderStates states) const;
+		inline void Update(sf::Time dt);
+
+	private:
+		sf::Sprite sprite;
+		sf::Texture texture;
+		sf::Shader shader;
+	};
+
+	StarField::StarField(sf::RenderWindow const &wnd)
 	{
-		class StarField: public sf::Drawable
+		auto winSize = wnd.getSize();
+
+		if (!shader.loadFromFile("space.frag", sf::Shader::Fragment))
 		{
-		public:
-			StarField(sf::RenderWindow const &wnd);
-			~StarField();
-			virtual void draw(sf::RenderTarget &wnd, sf::RenderStates states) const;
-			void Update(sf::Time dt);
-		private:
-			sf::Sprite sprite;
-			sf::Texture texture;
-			sf::Shader shader;
-		};
+		}
+
+		texture.create(winSize.x, winSize.y);
+		sprite.setTexture(texture);
+	}
+
+	inline void StarField::draw(sf::RenderTarget &wnd, sf::RenderStates states) const
+	{
+		wnd.draw(sprite, &shader);
+	}
+
+	inline void StarField::Update(sf::Time dt)
+	{
+		static float time = 0;
+		time += dt.asSeconds();
+		shader.setUniform("time", time);
 	}
 }
+
+#endif
