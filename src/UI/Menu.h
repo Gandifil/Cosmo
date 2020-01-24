cfab793eee54b0425b7b162f7b6e57bb7aa32001
Gandifil <Gandifil@DESKTOP-8DCQQ76>
2020-01-24 19:34:53 +0300

    Refactor Menu file.

--- src/UI/Menu.h
+++ src/UI/Menu.h
@@ -1,27 +1,33 @@
-#pragma once
+//
+// Created by Gandifil on 24.01.2020.
+//
+
+#ifndef COSMO_UI_MENU
+#define COSMO_UI_MENU
+
 #include "Scene.h"
 #include <TGUI/TGUI.hpp>
 
-namespace Cosmo
-{
-	namespace UI {
-		class Menu : public Scene {
-		public:
-			Menu(sf::RenderWindow &wnd) : gui{wnd} {};
+namespace Cosmo::UI {
+	class Menu : public Scene {
+	public:
+		Menu(sf::RenderWindow &wnd) : gui{wnd} {};
 
-			virtual int HandleEvent(sf::Event event) override {
-				gui.handleEvent(event);
-				return 1;
-			};
-
-			virtual void Update(sf::Time dt) override {};
-
-			virtual void Render() override {
-				gui.draw();
-			}
-
-		protected:
-			tgui::Gui gui;
+		virtual int HandleEvent(sf::Event event) override {
+			gui.handleEvent(event);
+			return 1;
 		};
-	}
+
+		virtual void Update(sf::Time dt) override {
+			gui.updateTime(dt);
+		};
+
+		virtual void Render() override {
+			gui.draw();
+		}
+
+	protected:
+		tgui::Gui gui;
+	};
 }
+#endif
