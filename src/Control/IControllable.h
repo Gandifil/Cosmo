47187e6cff85b57d34018c6fa5f7fca9b6341ee1
Gandifil <Gandifil@DESKTOP-8DCQQ76>
2020-01-25 05:11:37 +0300

    Refactor

--- src/Control/IControllable.h
+++ src/Control/IControllable.h
@@ -1,32 +1,34 @@
-#pragma once
-#include <SFML\System.hpp>
+//
+// Created by Gandifil on 24.01.2020.
+//
 
-namespace Cosmo
-{
-	namespace Control {
+#ifndef COSMO_CONTROL_ICONTROLLABLE
+#define COSMO_CONTROL_ICONTROLLABLE
 
-		/// <summary>
-		///  This union keep a data about pressed buttons on controller.
-		/// </summary>
-		struct Directions {
-			int top : 1;
-			int down : 1;
-			int left : 1;
-			int right : 1;
-		};
+namespace Cosmo::Control {
+	/// <summary>
+	///  This union keep a data about pressed buttons on controller.
+	/// </summary>
+	struct Directions {
+		int top : 1;
+		int down : 1;
+		int left : 1;
+		int right : 1;
+	};
 
-		/// <summary>
-		///  Interface for game object which may be under control by player.
-		/// </summary>
-		class IControllable
-		{
-		public:
-			virtual void MainShoot() = 0;
-			virtual void AltShoot() = 0;
-			virtual void MainAbility() = 0;
-			virtual void AltAbility() = 0;
+	/// <summary>
+	///  Interface for game object which may be under control by player.
+	/// </summary>
+	class IControllable
+	{
+	public:
+		virtual void MainShoot() = 0;
+		virtual void AltShoot() = 0;
+		virtual void MainAbility() = 0;
+		virtual void AltAbility() = 0;
 
-			Directions directions;
-		};
-	}
+		Directions directions;
+	};
 }
+
+#endif // COSMO_CONTROL_ICONTROLLABLE
