e30890513cf02dfd5b064aa93c0e917381020123
Max Kopjev <GandifilMK@gmail.com>
2020-07-06 15:23:52 +0300

    Change weapon parameters.

--- res/entitytypes.lua
+++ res/entitytypes.lua
@@ -1,9 +1,9 @@
 texture = TextureBox.new("cruiser01.png")
 laser_texture = TextureBox.new("laserCruiser01.png")
 
-bullet01 = BulletBox.new(laser_texture, 100, 200)
-weap1 = WeaponBox.new(bullet01, 0.25, VectorBox.new(-32, 0), VectorBox.new(0, -120))
-weap2 = WeaponBox.new(bullet01, 0.25, VectorBox.new(32, 0), VectorBox.new(0, -120))
+bullet01 = BulletBox.new(laser_texture, 100, 700)
+weap1 = WeaponBox.new(bullet01, 0.25, VectorBox.new(-32, -30), VectorBox.new(0, -120))
+weap2 = WeaponBox.new(bullet01, 0.25, VectorBox.new(32, -30), VectorBox.new(0, -120))
 speed = SpeedBox.new(300, 170, 290)
 cruiserStarship = StarshipBox.new(texture, speed, 1000)
 addBox("first", CruiserBox.new(cruiserStarship, weap1, weap2))
